4aeb642a70ebd38bfb43a891914f7ef652b871df
Moritz <moritz.reichelt2001@gmail.com>
2022-08-13 16:46:29 +0200

    add Entity

---
+++ src/Entity/DatabaseSync.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DatabaseSyncRepository;
+use Doctrine\DBAL\Types\BlobType;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DatabaseSyncRepository::class)]
+class DatabaseSync
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $api_key = null;
+
+    #[ORM\Column]
+    private ?BlobType $client_db = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getApiKey(): ?string
+    {
+        return $this->api_key;
+    }
+
+    public function setApiKey(string $api_key): self
+    {
+        $this->api_key = $api_key;
+
+        return $this;
+    }
+
+    public function getClientDb(): ?BlobType
+    {
+        return $this->client_db;
+    }
+
+    public function setClientDb(?BlobType $client_db): self
+    {
+        $this->client_db = $client_db;
+
+        return $this;
+    }
+}
